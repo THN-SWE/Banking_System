404f0480fb613fddb2e832b0ad6ec212e9c9964f
thulasikan <thulasikanswe@gmail.com>
2023-08-13 21:07:47 +0530

    nothing left to do. but might still be some bugs. need testing

--- addon.js
+++ addon.js
@@ -4,7 +4,6 @@
 //     e.returnValue = ''
 //   });
 
-
 // window.onload = () => {
 //   user_db = JSON.parse(localStorage.user_db);
 // };
@@ -13,7 +12,6 @@ let disp_user_name = document.getElementById("disp_user_name");
 disp_user_name.innerHTML = localStorage.disp_user_email;
 let user_balance = document.getElementById("user_balance");
 user_balance.innerHTML = user_db[localStorage.user_uid].balance + " Rs";
-
 
 function widraw() {
   let widraw_amount = document.getElementById("widraw_amount");
@@ -34,7 +32,6 @@ function widraw() {
 }
 
 function deposit() {
- 
   let deposit_amount = document.getElementById("deposit_amount");
   if (deposit_amount.value < 0 || deposit_amount.value == "e") {
     alert("Invalid Amount");
@@ -50,6 +47,45 @@ function deposit() {
     deposit_amount.value = "";
     localStorage.setItem("user_db", JSON.stringify(user_db));
   }
+}
+
+//function to find specific email (to find ace)
+
+function transfer() {
+  let rec_email = document.getElementById("reciever_email");
+  let transfer_amount = document.getElementById("transfer_amount");
+  if (transfer_amount.value < 0 || transfer_amount.value === "e") {
+    alert("Invalid Amount");
+
+    return;
+  }
+  for (const key in user_db) {
+    if (
+      user_db[key].email === rec_email.value &&
+      parseInt(transfer_amount.value) <= parseInt(user_balance.innerHTML)
+    ) {
+      console.log("tranfered !");
+
+      user_db[localStorage.user_uid].balance -= transfer_amount.value;
+      user_balance.innerHTML = user_db[localStorage.user_uid].balance + " Rs";
+
+      user_db[key].balance = parseInt(user_db[key].balance)+ parseInt(transfer_amount.value) ;
+      transfer_amount.value = "";
+
+      localStorage.setItem("user_db", JSON.stringify(user_db));
+
+      return;
+    } 
+    
+  }
+  alert('Invalid Inputs')
+
+}
+function logout(){
+  window.location="index.html";
+  user_db[localStorage.user_uid].loggedin = false;
+  localStorage.setItem("user_db", JSON.stringify(user_db));
+
 }
 // NOTE
 // localStorage.user_db;
